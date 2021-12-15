8eddd117161ff3ebf0630e2bba49e051d7a901e9
TypingCat <jinhong.p.noh@gmail.com>
2021-12-15 16:25:59 +0900

    Distribute parameters

--- scripts/augmentation.py
+++ scripts/augmentation.py
@@ -13,16 +13,9 @@ from collections import defaultdict
 
 class Augmentation:
     '''LiDAR point cloud augmentation'''
-    
     point_cloud_format = '=ffffHf'
-    # target_ring = [6]
-    window_size = 10    
+    window_size = 10
     lidar_height = 0.35
-    dphi_min = 0.05
-    ground_min, ground_max = -0.05, 0.2
-    i_min, i_accept = 30, 50
-    d_max = 1.0
-    dn_dphi = 5/(math.pi/180.)
 
     def __init__(self):
         self.signal_loss_range_publisher = rospy.Publisher("signal_loss_range", Marker, queue_size=10)
@@ -31,67 +24,33 @@ class Augmentation:
 
     def point_cloud_callback(self, data):
         points, bump_points = self.get_points(data)
-        # print(len(points))
-        # print(data.height, data.width)
-        # print(data.point_step, data.row_step)
-        return
 
         # Algorithm 2
         points = self.remove_outlier(points)
         
         # Algorithm 3
         signal_loss_range = self.search_signal_loss_range(points)
-
-        # Add bump points to the result
-        for p in bump_points:
-            data.data += struct.pack(self.point_cloud_format,
-                    p[0], p[1], 0., p[3], 16, p[5])
-        data.width += len(bump_points)
         
-        # Add augmentation points to the result
-        for p in signal_loss_range:
-            n = int((p[0][6] - p[1][6]) * self.dn_dphi)
-            for rate in [float(r)/n for r in range(1, n)]:
-                data.data += struct.pack(self.point_cloud_format,
-                    p[0][0] + rate*(p[1][0] - p[0][0]),
-                    p[0][1] + rate*(p[1][1] - p[0][1]),
-                    p[0][2] + rate*(p[1][2] - p[0][2]),
-                    p[0][3] + rate*(p[1][3] - p[0][3]),
-                    p[0][4],
-                    p[0][5] + rate*(p[1][5] - p[0][5]))
-            data.width += n - 1
-        data.row_step = data.width * data.point_step
+        # Augment points to the result
+        interpolation_points = self.interpolate_range(signal_loss_range)
+        data = self.augment_points(data, interpolation_points + bump_points)
+        
+        # Publish results
         self.point_cloud_publisher.publish(data)
+        self.signal_loss_range_publisher.publish(
+            self.create_range_marker(signal_loss_range, data.header))
         
-        # Publish marker
-        marker = Marker()
-        marker.header = data.header
-        marker.ns = "signal_loss_range"
-        marker.type = Marker.LINE_LIST
-        marker.action = Marker.MODIFY
-        marker.pose.orientation.w = 1
-        marker.scale.x = 0.01
-        marker.color.r = 0
-        marker.color.g = 0
-        marker.color.b = 1
-        marker.color.a = 0.5
-        marker.points = []
-        [marker.points.extend([Point(p[0][0], p[0][1], p[0][2]), Point(p[1][0], p[1][1], p[1][2])]) for p in signal_loss_range]
-        self.signal_loss_range_publisher.publish(marker)
-        
-    def get_points(self, data):
+    def get_points(self, data, target_ring=[], ground_min=-0.05, ground_max=0.2, i_min=30):
         points, bump_points = defaultdict(list), []
         for step in range(0, data.row_step, data.point_step):
-            x, y, z, i, ring, t = struct.unpack_from(self.point_cloud_format, data.data, step)            
-            # if ring not in self.target_ring: continue   # limit input ring
-
-            # print(x, y, z, i, ring, t)
+            x, y, z, i, ring, t = struct.unpack_from(self.point_cloud_format, data.data, step)
+            if len(target_ring)!=0 and ring not in target_ring: continue
             
             # Get bump points
             h = z + self.lidar_height
-            if h < self.ground_min: continue            # Remove ground reflection
-            if h < self.ground_max and i > self.i_min:
-                bump_points.append((x, y, z, i, ring, t))
+            if h < ground_min: continue     # Remove ground reflection
+            if h < ground_max and i > i_min:
+                bump_points.append((x, y, 0., i, 16, t))
             
             # Get normal points
             phi = math.atan2(y, x)
@@ -130,24 +89,23 @@ class Augmentation:
 
         return points
 
-    def search_signal_loss_range(self, points):
+    def search_signal_loss_range(self, points, dphi_min=0.05, d_max=1.0, i_accept=50):
         signal_loss_range = []
         for ring in points.keys():
             for idx in range(len(points[ring])-1):
                 p0, p1 = points[ring][idx], points[ring][idx+1]
                 
                 # Check interpolation requirements
-                if p0[6] - p1[6] < self.dphi_min: continue
-                # if p0[2] < self.ground_max or p1[2] < self.ground_max: continue
-                if math.sqrt(math.pow(p0[0] - p1[0], 2) + math.pow(p0[1] - p1[1], 2)) > self.d_max: continue
+                if p0[6] - p1[6] < dphi_min: continue
+                if math.sqrt(math.pow(p0[0] - p1[0], 2) + math.pow(p0[1] - p1[1], 2)) > d_max: continue
                 
                 # Update endpoints with acceptable intensity points
                 for i in range(idx, max(idx-self.window_size, 0), -1):
-                    if points[ring][i][3] > self.i_accept:
+                    if points[ring][i][3] > i_accept:
                         p0 = points[ring][i]
                         break
                 for i in range(idx+1, min(idx+1+self.window_size, len(points[ring]))):
-                    if points[ring][i][3] > self.i_accept:
+                    if points[ring][i][3] > i_accept:
                         p1 = points[ring][i]
                         break
 
@@ -155,6 +113,46 @@ class Augmentation:
                 signal_loss_range.append((p0, p1))
 
         return signal_loss_range
+    
+    def augment_points(self, data, points):
+        for p in points:
+            data.data += struct.pack(self.point_cloud_format,
+                    p[0], p[1], p[2], p[3], p[4], p[5])
+
+        data.width += len(points)
+        return data
+
+    def interpolate_range(self, signal_loss_range, dn_dphi=5/(math.pi/180.)):
+        points = []
+        for p in signal_loss_range:
+            n = int((p[0][6] - p[1][6]) * dn_dphi)
+            for rate in [float(r)/n for r in range(1, n)]:
+                points.append((
+                    p[0][0] + rate*(p[1][0] - p[0][0]),
+                    p[0][1] + rate*(p[1][1] - p[0][1]),
+                    p[0][2] + rate*(p[1][2] - p[0][2]),
+                    p[0][3] + rate*(p[1][3] - p[0][3]),
+                    p[0][4],
+                    p[0][5] + rate*(p[1][5] - p[0][5])))
+
+        return points
+    
+    def create_range_marker(self, signal_loss_range, header):
+        marker = Marker()
+        marker.header = header
+        marker.ns = "signal_loss_range"
+        marker.type = Marker.LINE_LIST
+        marker.action = Marker.MODIFY
+        marker.pose.orientation.w = 1
+        marker.scale.x = 0.01
+        marker.color.r = 0
+        marker.color.g = 0
+        marker.color.b = 1
+        marker.color.a = 0.5
+        marker.points = []
+        [marker.points.extend([Point(p[0][0], p[0][1], p[0][2]), Point(p[1][0], p[1][1], p[1][2])]) for p in signal_loss_range]
+
+        return marker
 
 
 if __name__ == '__main__':
